8b23abe4652ac662490e9f07c395c1d9a8150850
zack <zphricz@gmail.com>
2015-11-07 14:09:34 -0800

    -Code formatting

--- Game.h
+++ Game.h
@@ -7,33 +7,33 @@
 #include "Vec.h"
 
 class Game {
-private:
-  SoftScreen *scr;
-  int map_width;
-  int map_height;
-  float direction;
-  float height;
-  float pitch;
-  float fov;
-  const float plane_width;
-  const float plane_height;
-  float plane_distance;
-  Linear::Vec2f position;
-  std::vector<char> map;
-  std::vector<SDL_Color> colors;
-  SDL_Color ceiling_color;
-  SDL_Color floor_color;
-  bool running;
+  private:
+    SoftScreen *scr;
+    int map_width;
+    int map_height;
+    float direction;
+    float height;
+    float pitch;
+    float fov;
+    const float plane_width;
+    const float plane_height;
+    float plane_distance;
+    Linear::Vec2f position;
+    std::vector<char> map;
+    std::vector<SDL_Color> colors;
+    SDL_Color ceiling_color;
+    SDL_Color floor_color;
+    bool running;
 
-  char &map_at(int x, int y);
-  void render_slice(int i);
-  void handle_input();
-  void draw_game();
+    char &map_at(int x, int y);
+    void render_slice(int i);
+    void handle_input();
+    void draw_game();
 
-public:
-  Game(SoftScreen *scr, const char *map_name);
-  ~Game();
-  void run();
+  public:
+    Game(SoftScreen *scr, const char *map_name);
+    ~Game();
+    void run();
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -8,53 +8,53 @@
 using namespace std;
 
 static void error(char *name) {
-  printf("Usage: %s [Screen_x Screen_y] [map_name] [num_threads]\n", name);
-  exit(1);
+    printf("Usage: %s [Screen_x Screen_y] [map_name] [num_threads]\n", name);
+    exit(1);
 }
 
 int main(int argc, char *argv[]) {
-  SDL_DisplayMode display;
-  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-    exit(1);
-  }
-  SDL_GetCurrentDisplayMode(0, &display);
-  atexit(SDL_Quit);
-
-  int screen_width = display.w;
-  int screen_height = display.h;
-  int num_threads = thread::hardware_concurrency();
-  string map_name = "map.txt";
-
-  switch (argc) {
-  case 5:
-    num_threads = atoi(argv[4]);
-    if (num_threads <= 0) {
-      error(argv[0]);
+    SDL_DisplayMode display;
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        exit(1);
     }
-  case 4:
-    map_name = argv[3];
-  case 3:
-    screen_width = atoi(argv[1]);
-    screen_height = atoi(argv[2]);
-    if (screen_width <= 0 || screen_height <= 0) {
-      error(argv[0]);
+    SDL_GetCurrentDisplayMode(0, &display);
+    atexit(SDL_Quit);
+
+    int screen_width = display.w;
+    int screen_height = display.h;
+    int num_threads = thread::hardware_concurrency();
+    string map_name = "map.txt";
+
+    switch (argc) {
+    case 5:
+        num_threads = atoi(argv[4]);
+        if (num_threads <= 0) {
+            error(argv[0]);
+        }
+    case 4:
+        map_name = argv[3];
+    case 3:
+        screen_width = atoi(argv[1]);
+        screen_height = atoi(argv[2]);
+        if (screen_width <= 0 || screen_height <= 0) {
+            error(argv[0]);
+        }
+    case 1:
+        break;
+    default:
+        error(argv[0]);
     }
-  case 1:
-    break;
-  default:
-    error(argv[0]);
-  }
 
-  bool full_screen;
-  if (screen_width == display.w && screen_height == display.h) {
-    full_screen = true;
-  } else {
-    full_screen = false;
-  }
+    bool full_screen;
+    if (screen_width == display.w && screen_height == display.h) {
+        full_screen = true;
+    } else {
+        full_screen = false;
+    }
 
-  Threadpool::set_num_threads(num_threads);
-  SoftScreen scr(screen_width, screen_height, "Raycaster", full_screen, true);
-  Game g(&scr, map_name.c_str());
-  g.run();
-  return 0;
+    Threadpool::set_num_threads(num_threads);
+    SoftScreen scr(screen_width, screen_height, "Raycaster", full_screen, true);
+    Game g(&scr, map_name.c_str());
+    g.run();
+    return 0;
 }
